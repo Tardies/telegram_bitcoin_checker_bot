d84a1ec3076badbc59243971b21f1ac438673b63
Andrey <32172020+Tardies@users.noreply.github.com>
2022-05-26 20:16:32 +0200

    Add files via upload

--- bitcoin_bot.py
+++ bitcoin_bot.py
@@ -3,12 +3,25 @@ import requests
 from time import sleep
 from bs4 import BeautifulSoup
 from datetime import datetime
+
 bitcoin = 0.01301
+bot = telebot.TeleBot("5374222900:AAHSy4EmHhQ2hVn3mQESxQKajeAGa71FA0U")
 
 def toFixed(numObj, digits=2):
     return f"{numObj:.{digits}f}"
 
-bot = telebot.TeleBot("5374222900:AAHSy4EmHhQ2hVn3mQESxQKajeAGa71FA0U")
+def bitcoin_scrap():
+    r = requests.get('https://crypto.com/price/bitcoin')
+    soup = BeautifulSoup(r.content, 'html.parser')
+    s = soup.find('h2', class_='chakra-heading css-64zram')
+    content = s.find_all()
+    c = str(content[1])
+    c = c[39:48]
+    c = c.replace(',', '')
+    c = float(c)
+    money = c * bitcoin
+    output = f'Course: ${c}\nBitcoins: {bitcoin}\nMoney: ${toFixed(money)}'
+    return output
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -21,17 +34,7 @@ def send_help(message):
 
 @bot.message_handler(commands=['bitcoin_check'])
 def send_bitcoin_check(message):
-    r = requests.get('https://crypto.com/price/bitcoin')
-    soup = BeautifulSoup(r.content, 'html.parser')
-    s = soup.find('h2', class_='chakra-heading css-64zram')
-    content = s.find_all()
-    c = str(content[1])
-    c = c[39:48]
-    c = c.replace(',', '')
-    c = float(c)
-    money = c * bitcoin
-    output = f'Course: ${c}\nBitcoins: {bitcoin}\nMoney: ${toFixed(money)}'
-    bot.reply_to(message, output)
+    bot.reply_to(message, bitcoin_scrap())
 
 @bot.message_handler(commands=['bitcoin_check_infinite'])
 def send_bitcoin_check_infinite(message):
@@ -39,34 +42,14 @@ def send_bitcoin_check_infinite(message):
         now = datetime.now()
         current_time = int(now.strftime("%H"))
         if 6 < current_time and current_time < 23:
-            r = requests.get('https://crypto.com/price/bitcoin')
-            soup = BeautifulSoup(r.content, 'html.parser')
-            s = soup.find('h2', class_='chakra-heading css-64zram')
-            content = s.find_all()
-            c = str(content[1])
-            c = c[39:48]
-            c = c.replace(',', '')
-            c = float(c)
-            money = c * bitcoin
-            output = f'Course: ${c}\nBitcoins: {bitcoin}\nMoney: ${toFixed(money)}'
-            bot.reply_to(message, output)
+            bot.reply_to(message, bitcoin_scrap())
             sleep(60*15) #every 15 minutes
         else:
             output_arr = "Night time:\n"
             for _ in range(7):
-                r = requests.get('https://crypto.com/price/bitcoin')
-                soup = BeautifulSoup(r.content, 'html.parser')
-                s = soup.find('h2', class_='chakra-heading css-64zram')
-                content = s.find_all()
-                c = str(content[1])
-                c = c[39:48]
-                c = c.replace(',', '')
-                c = float(c)
-                money = c * bitcoin
                 time = now.strftime("%H:%M")
-                output = f'{time}:\nCourse: ${c}\nBitcoins: {bitcoin}\nMoney: ${toFixed(money)}\n\n'
-                output_arr += output
-                sleep(60*60) #every hour
+                output_arr += f'{time}:\n{bitcoin_scrap()}\n\n'
+                sleep(2) #every hour
             bot.reply_to(message, output_arr)
 
 @bot.message_handler(func=lambda message: True)
