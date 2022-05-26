a83eb7e552306f06f703d68ccfe08dac9d5f5776
Andrey <32172020+Tardies@users.noreply.github.com>
2022-05-26 19:56:33 +0200

    Add files via upload

--- bitcoin_bot.py
+++ bitcoin_bot.py
@@ -2,6 +2,7 @@ import telebot
 import requests
 from time import sleep
 from bs4 import BeautifulSoup
+from datetime import datetime
 bitcoin = 0.01301
 
 def toFixed(numObj, digits=2):
@@ -35,18 +36,38 @@ def send_bitcoin_check(message):
 @bot.message_handler(commands=['bitcoin_check_infinite'])
 def send_bitcoin_check_infinite(message):
     while True:
-        r = requests.get('https://crypto.com/price/bitcoin')
-        soup = BeautifulSoup(r.content, 'html.parser')
-        s = soup.find('h2', class_='chakra-heading css-64zram')
-        content = s.find_all()
-        c = str(content[1])
-        c = c[39:48]
-        c = c.replace(',', '')
-        c = float(c)
-        money = c * bitcoin
-        output = f'Course: ${c}\nBitcoins: {bitcoin}\nMoney: ${toFixed(money)}'
-        bot.reply_to(message, output)
-        sleep(60*15) #every 15 minutes
+        now = datetime.now()
+        current_time = int(now.strftime("%H"))
+        if 6 < current_time and current_time < 23:
+            r = requests.get('https://crypto.com/price/bitcoin')
+            soup = BeautifulSoup(r.content, 'html.parser')
+            s = soup.find('h2', class_='chakra-heading css-64zram')
+            content = s.find_all()
+            c = str(content[1])
+            c = c[39:48]
+            c = c.replace(',', '')
+            c = float(c)
+            money = c * bitcoin
+            output = f'Course: ${c}\nBitcoins: {bitcoin}\nMoney: ${toFixed(money)}'
+            bot.reply_to(message, output)
+            sleep(60*15) #every 15 minutes
+        else:
+            output_arr = "Night time:\n"
+            for _ in range(7):
+                r = requests.get('https://crypto.com/price/bitcoin')
+                soup = BeautifulSoup(r.content, 'html.parser')
+                s = soup.find('h2', class_='chakra-heading css-64zram')
+                content = s.find_all()
+                c = str(content[1])
+                c = c[39:48]
+                c = c.replace(',', '')
+                c = float(c)
+                money = c * bitcoin
+                time = now.strftime("%H:%M")
+                output = f'{time}:\nCourse: ${c}\nBitcoins: {bitcoin}\nMoney: ${toFixed(money)}\n\n'
+                output_arr += output
+                sleep(60*60) #every hour
+            bot.reply_to(message, output_arr)
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
